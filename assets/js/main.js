e5395e462870233c06925ccd6a587730c1a7c17b
Manoj S <131352636+Manoj-S375@users.noreply.github.com>
2024-08-08 20:39:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Python Coder","Django Developer"],
+  strings : ["Python Coder","Web Developer","Django Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
